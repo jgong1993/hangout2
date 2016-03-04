6a4e631cb3ff491eeafda3539c70a879f823ec40
Johnson Gong <jogong@ucsd.edu>
2016-03-03 17:43:53 -0800

    still trying to find out why it wont push image

--- routes/gallery.js
+++ routes/gallery.js
@@ -21,7 +21,7 @@ exports.viewGallery = function(req, res){
 exports.addPhoto = function(req,res){
 	 upload(req,res,function(err) {
         if(err) {
-            console.log(err);
+            console.log("There appears to be an error: " + err);
             return res.end("Error uploading file.");
         }
         var origName = req.body.group;
