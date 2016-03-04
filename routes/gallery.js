c191feb2ee7b6ff56b9d9e876f180ec6811268a9
Johnson Gong <jogong@ucsd.edu>
2016-03-03 18:01:30 -0800

    comment out gallery

--- routes/gallery.js
+++ routes/gallery.js
@@ -19,19 +19,18 @@ exports.viewGallery = function(req, res){
 };
 
 exports.addPhoto = function(req,res){
-          console.log(req);
-
 	 upload(req,res,function(err) {
         if(err) {
             return res.end("Error uploading file.");
-        }        var origName = req.body.group;
-        for(var i = 0; i < eventTest.groups2.length; i++) {
-  			  if(eventTest["groups2"][i].group == origName) {
-    				var newTarget = "{\"image\" : \"./uploads/"+req.file.originalname+"\"}";
-    				eventTest["groups2"][i]["imageURL"].push(JSON.parse(newTarget));
-    				break;
-  			  }
-  		  }       
+        }
+      //   var origName = req.body.group;
+      //   for(var i = 0; i < eventTest.groups2.length; i++) {
+  			 //  if(eventTest["groups2"][i].group == origName) {
+    		// 		var newTarget = "{\"image\" : \"./uploads/"+req.file.originalname+"\"}";
+    		// 		eventTest["groups2"][i]["imageURL"].push(JSON.parse(newTarget));
+    		// 		break;
+  			 //  }
+  		  // }       
 		    res.redirect('gallery'); 
     });
 };
